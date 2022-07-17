33ac92b27b3b26e27cc48fb212003dc139b55636
CianMcDonald <cianmcdonald2001@gmail.com>
2022-07-17 23:40:27 +0100

    added h1 and form

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="contianer is-fluid">
+  <h1 class="title is-1 has-text-centered">Welcome to SvelteKit</h1>
+  <div class="field">
+    <div class="control">
+        <label class="label">Summoner Name</label>
+      <input class="input" type="text" placeholder="Summoner Name">
+    </div>
+  </div>
+</div>
